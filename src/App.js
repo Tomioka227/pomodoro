2e96fc2a15ca6428f2980ead2ac9ec9b654f5be8
Tomioka227 <giyu999888@gmail.com>
2022-02-11 06:52:09 -0800

    pomodoro --2

--- src/App.js
+++ src/App.js
@@ -1,103 +1,96 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import FruitList from "./components";
-import FruitListItem from "./components"
-import './App.css';
+import FruitListItem from "./components";
+import "./App.css";
 
 let intervalId = 0;
 
 const music = new Audio("../public/ring.wav");
 const breakTime = 5;
 const workTime = 25;
-let data = [
-
-];
-
+let data = [];
 
 function TimerComponent() {
-  
-  
-  
   let [second, setSecond] = useState(0);
   let [minute, setMinute] = useState(25);
   let [btnText, setBtnText] = useState("Start");
   let [taskName, setTaskName] = useState("fdsafsdafsda");
-  let [mylist, setMyList] = useState([{id:1, name:"fdsa"}]);
+  let [mylist, setMyList] = useState([{ id: 1, name: "fdsa" }]);
   // let taskBar = <h1>fdsafsadfsad</h1>;
- 
-  useEffect( () => {
+
+  useEffect(() => {
     setBtnText(!intervalId ? "Start" : "Pause");
   }, [intervalId]);
 
   const resetComponents = (flag) => {
     setBtnText(!flag ? "Start" : "Pause");
-  }
+  };
 
   const countDown = () => {
-    if(!second) {
+    if (!second) {
       second = 59;
       minute--;
-    }
-    else second--;
+    } else second--;
     setSecond(second);
     setMinute(minute);
-    if(!second && !minute) {
+    if (!second && !minute) {
       music.play();
-      debugger
+      debugger;
       mylist.shift(mylist.indexOf(0));
       setMyList(mylist);
       handleResetClick();
     }
-  }
+  };
 
   const handleFireClick = () => {
-    !intervalId ? intervalId = setInterval(countDown, 100) : intervalId = clearInterval(intervalId);
+    !intervalId
+      ? (intervalId = setInterval(countDown, 1000))
+      : (intervalId = clearInterval(intervalId));
     resetComponents(intervalId);
-  }
+  };
 
   const handleResetClick = () => {
     intervalId = clearInterval(intervalId);
     setSecond(0);
     setMinute(25);
-  }
+  };
 
   const handleBreakClick = () => {
-    if(!intervalId)
-      setMinute(1);
-  }
+    if (!intervalId) setMinute(1);
+  };
 
   const handleWorkClick = () => {
-    if(!intervalId) setMinute(25);
-  }
+    if (!intervalId) setMinute(25);
+  };
 
   const handleAddClick = () => {
     const ulData = {
       id: data.length + 1,
-      name: taskName
+      name: taskName,
     };
-    debugger
+    debugger;
     data.push(ulData);
     setMyList(data);
     mylist = data;
     handleFireClick();
     handleResetClick();
-  }
+  };
 
   // useEffect( () => {
-  //   list = (data);  
+  //   list = (data);
   //   setList(list);
   // }, [data])
 
   const handleInputChange = (event) => {
     const target = event.target;
     taskName = target.value;
-    
-  }
+  };
 
   return (
     <div>
-      <label>{ minute }</label>
+      <label>{minute}</label>
       <label>:</label>
-      <label>{ second }</label>
+      <label>{second}</label>
       <input onChange={handleInputChange}></input>
       <button onClick={handleAddClick}>Add</button>
       <button onClick={handleBreakClick}>Break Time</button>
@@ -106,17 +99,13 @@ function TimerComponent() {
       <button onClick={handleResetClick}>Reset</button>
       <FruitList fruits={mylist} />
     </div>
-  )
+  );
 }
 
-
-
-
 function App() {
-  
   return (
     <div className="App">
-      <TimerComponent />      
+      <TimerComponent />
     </div>
   );
 }
